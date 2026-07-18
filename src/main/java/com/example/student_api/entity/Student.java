6f02a0eb0d990e73257978a422bdbc0827899d46
rajesh-r22 <itsmerajeshreddy01@gmail.com>
2026-07-18 16:52:24 +0530

    Add student class in entity package and give feilds to it with proper validations

---
+++ src/main/java/com/example/student_api/entity/Student.java
@@ -0,0 +1,26 @@
+package com.example.student_api.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "students")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long  id;
+
+    @Column(nullable = false,length = 100)
+    private String name;
+
+    @Column(nullable = false,length = 100,unique = true)
+    private String email;
+
+    private Integer age;
+
+}

ddc3f9240ca8b02ad2068b12305713477663f637
rajesh-r22 <itsmerajeshreddy01@gmail.com>
2026-07-27 21:12:20 +0530

    Add fields to the Address dto with validations

--- src/main/java/com/example/student_api/dto/AddressDto.java
+++ src/main/java/com/example/student_api/dto/AddressDto.java
@@ -1,5 +1,14 @@
 package com.example.student_api.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
 public class AddressDto {
+    @NotBlank(message = "City cannot be blank")
+    private String city;
+
+    @NotBlank(message = "PinCode is required")
+    @Pattern(regexp = "^[1-9][0-9]{5}$" , message = "PinCode must be valid 6 digit Indian pinCode")
+    private String pinCode;
 
 }

03aa67c0134e46fb3d96e64678350a338964390c
rajesh-r22 <itsmerajeshreddy01@gmail.com>
2026-07-29 20:15:28 +0530

    Feat: create mapstruct for mapping from dto to entity and entity to dto , and mapstruct provides more relaibilitya nd clean code , it work at compile time

---
+++ src/main/java/com/example/student_api/mapper/StudentMapper.java
@@ -0,0 +1,19 @@
+package com.example.student_api.mapper;
+
+import com.example.student_api.dto.StudentDto;
+import com.example.student_api.entity.Student;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+
+@Mapper(componentModel = "spring")   // generates a Spring @Component implementation automatically
+public interface StudentMapper {
+
+    StudentDto toDTO(Student student);   // MapStruct matches passwordHash/createdAt/createdBy are simply absent on DTO — auto-ignored
+
+    @Mapping(target = "passwordHash", ignore = true)   // explicit — even though not needed here, shows intent
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "createdBy", ignore = true)
+    @Mapping(target = "id", ignore = true)              // id is DB-generated, never set from incoming DTO on create
+    Student toEntity(StudentDto dto);
+}

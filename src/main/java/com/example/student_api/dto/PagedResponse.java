b5524af0314cc6cad07233111d25b474af727717
rajesh-r22 <itsmerajeshreddy01@gmail.com>
2026-08-03 15:41:30 +0530

    Add feilds to aur custom wrapper class for clean ,stable JSON structure for your api

--- src/main/java/com/example/student_api/dto/PagedResponse.java
+++ src/main/java/com/example/student_api/dto/PagedResponse.java
@@ -1,4 +1,16 @@
 package com.example.student_api.dto;
 
-public class PagedResponse {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+public class PagedResponse<T> {
+    private List<T> content;
+    private int pageNumber;
+    private int pageSize;
+    private long totalPages;
+    private boolean isLast;
 }

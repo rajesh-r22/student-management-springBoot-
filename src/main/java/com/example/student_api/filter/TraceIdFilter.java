db7b4a11ab0c8fdd4b4da11d9e313a13b005a179
rajesh-r22 <itsmerajeshreddy01@gmail.com>
2026-08-20 23:04:54 +0530

    implement the TraceFilter class with filter from logging package

--- src/main/java/com/example/student_api/filter/TraceIdFilter.java
+++ src/main/java/com/example/student_api/filter/TraceIdFilter.java
@@ -2,7 +2,9 @@ package com.example.student_api.filter;
 
 import org.springframework.stereotype.Component;
 
+import java.util.logging.Filter;
+
 @Component
-public class TraceIdFilter {
+public class TraceIdFilter implements Filter {
 
 }

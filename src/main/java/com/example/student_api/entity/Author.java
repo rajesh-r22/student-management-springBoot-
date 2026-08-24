d7865471851d59128278eaeea9e39d5a23fb3a48
rajesh-r22 <itsmerajeshreddy01@gmail.com>
2026-08-24 22:22:56 +0530

    Add fields to Author entity and Add OneToMany on List<Book> books

--- src/main/java/com/example/student_api/entity/Author.java
+++ src/main/java/com/example/student_api/entity/Author.java
@@ -1,4 +1,29 @@
 package com.example.student_api.entity;
 
+import com.example.student_api.dto.BooksDto;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@Table(name="authors")
+@AllArgsConstructor
 public class Author {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @OneToMany(mappedBy = "author", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Books> books=new ArrayList<>();
+
 }

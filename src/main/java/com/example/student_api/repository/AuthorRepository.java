d2f945683f8f84cf43699e673b2a7a6b0148b5bd
rajesh-r22 <itsmerajeshreddy01@gmail.com>
2026-08-25 21:51:52 +0530

    create Author Repository and add method existByEmail

---
+++ src/main/java/com/example/student_api/repository/AuthorRepository.java
@@ -0,0 +1,8 @@
+package com.example.student_api.repository;
+
+import com.example.student_api.entity.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuthorRepository extends JpaRepository<Author,Long> {
+    boolean existByEmail(String email);
+}

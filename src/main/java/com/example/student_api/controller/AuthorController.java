9c9d939c0a6007e089c23c1ca77428d392bf03ef
rajesh-r22 <itsmerajeshreddy01@gmail.com>
2026-08-27 22:42:23 +0530

    Add postmapping end point in AuthorController

--- src/main/java/com/example/student_api/controller/AuthorController.java
+++ src/main/java/com/example/student_api/controller/AuthorController.java
@@ -1,10 +1,12 @@
 package com.example.student_api.controller;
 
 import com.example.student_api.dto.AuthorDetailsDto;
+import com.example.student_api.dto.BookDto;
 import com.example.student_api.response.ApiResponse;
 import com.example.student_api.service.AuthorService;
-import jakarta.persistence.GeneratedValue;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,6 +18,12 @@ public class AuthorController {
 
     @GetMapping("/{id}")
     public ResponseEntity<ApiResponse<AuthorDetailsDto>> findAuthorById(@PathVariable Long id) {
-        return ResponseEntity.ok(ApiResponse.success("Author Fetched",authorService.getAuthorWithBooks(id)));git
+        return ResponseEntity.ok(ApiResponse.success("Author Fetched",authorService.getAuthorWithBooks(id)));
+    }
+
+    @PostMapping("/{id}/books")
+    public ResponseEntity<ApiResponse<BookDto>> addBook(@PathVariable Long id, @Valid @RequestBody BookDto bookDto) {
+        BookDto created=authorService.addBookToAuthor(id, bookDto);
+        return new ResponseEntity<>(ApiResponse.success("Book added",created), HttpStatus.CREATED);
     }
 }

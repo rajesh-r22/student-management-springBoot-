5560d106bc1bf482857810db2d3d5a98399e647f
rajesh-r22 <itsmerajeshreddy01@gmail.com>
2026-09-10 23:43:52 +0530

    Add attributes to the student class of universityApi and add toString method to avoid recursion

--- src/main/java/com/example/student_api/universityApi/entity/Student.java
+++ src/main/java/com/example/student_api/universityApi/entity/Student.java
@@ -1,4 +1,26 @@
 package com.example.student_api.universityApi.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="uni_student")
+@Data
+@NoArgsConstructor
 public class Student {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Override
+    public String toString() {
+        return "Student{id=" + id + ", name='" + name + "'}"; // avoid recursion once Enrollment is added below
+    }
 }

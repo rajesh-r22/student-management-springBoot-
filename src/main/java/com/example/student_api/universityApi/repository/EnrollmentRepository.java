b8ff789aefc4d551f0653de0f797da7b5524fcd5
rajesh-r22 <itsmerajeshreddy01@gmail.com>
2026-09-11 23:25:31 +0530

    add all the db methods for universityApi.repository interfaces

--- src/main/java/com/example/student_api/universityApi/repository/EnrollmentRepository.java
+++ src/main/java/com/example/student_api/universityApi/repository/EnrollmentRepository.java
@@ -3,5 +3,13 @@ package com.example.student_api.universityApi.repository;
 import com.example.student_api.universityApi.entity.Enrollment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface EnrollmentRepository extends JpaRepository<Enrollment, Long> {
+    boolean existsByUniversityStudentIdCourseId(Long universityStudentId ,Long courseId);
+    Optional<Enrollment> findByUniversityStudentIdCourseId(Long universityStudentId, Long courseId);
+
+    List<Enrollment> findByUniversityStudentId(Long universityStudentId);
+    List<Enrollment> findByCourseId(Long courseId);
 }
--- src/main/java/com/example/student_api/universityApi/repository/UniversityStudentRepository.java
+++ src/main/java/com/example/student_api/universityApi/repository/UniversityStudentRepository.java
@@ -4,4 +4,5 @@ import com.example.student_api.universityApi.entity.UniversityStudent;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UniversityStudentRepository extends JpaRepository<UniversityStudent, Long> {
+    boolean existsByEmail(String email);
 }

3571fc86c12b568aef6e640b803ae9aa945d3576
rajesh-r22 <itsmerajeshreddy01@gmail.com>
2026-09-15 21:22:39 +0530

    feat: create new Service package in universityApi and create service package , Add new class EnrollmentService interface and define the methods to implimenet

---
+++ src/main/java/com/example/student_api/universityApi/service/EnrollmentService.java
@@ -0,0 +1,11 @@
+package com.example.student_api.universityApi;
+
+import com.example.student_api.universityApi.dto.EnrollmentDto;
+
+import java.util.List;
+
+public interface EnrollmentService {
+    EnrollmentDto enrollStudent(Long studentId,Long courseId);
+    void unenrollStudent(Long studentId,Long courseId);
+    List<EnrollmentDto> getEnrollmentsForStudents(Long studentId);
+}

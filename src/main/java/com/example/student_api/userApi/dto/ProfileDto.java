a459965bb40cabaaffb10a6b174be22dce20f7f2
rajesh-r22 <itsmerajeshreddy01@gmail.com>
2026-09-19 23:50:09 +0530

    create dto for User and Profile class

---
+++ src/main/java/com/example/student_api/userApi/dto/ProfileDto.java
@@ -0,0 +1,10 @@
+package com.example.student_api.userApi.dto;
+
+import lombok.Data;
+
+@Data
+public class ProfileDto {
+    private Long id;
+    private String bio;
+    private String phoneNumber;
+}
---
+++ src/main/java/com/example/student_api/userApi/dto/UserDto.java
@@ -0,0 +1,10 @@
+package com.example.student_api.userApi.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDto {
+    private Long id;
+    private String userName;
+    private String email;
+}
